88d8a058b69c8ca221aa294b5e58ddd30a01f4c2
Yassine Layachi <yassinelayachi@outlook.com>
2021-08-25 11:09:12 +0200

    array ex finished

--- 03-docker-environment/src/array/associative_array.php
+++ 03-docker-environment/src/array/associative_array.php
@@ -1,12 +1,12 @@
 <?php
- 
+
  $me=array(
      "Firstname" => "Yassine",
      "Lastname"  => "Layachi",
      "Age" => 26,
      "Season" => "Summer",
      "Soccer" => true,
-    //  "favorite_movies"=> ["Heat","Green Line","The 7 mercenaries","Rush Hour","The terminal","Bourne","Inside Man","Malcolm X"]
+     "favorite_movies"=> ["Heat","Green Line","The 7 mercenaries","Rush Hour","The terminal","Bourne","Inside Man","Malcolm X"]
  );
 
  $me["hobbies"]=["Reading","Politics","Sport","History","Movies"];
@@ -24,9 +24,53 @@ $brother["hobbies"]=["Manga","Video Games","Sport","Fast-food"];
 
 $me["brother"]=$brother;
 
+$b_hobbies= count($brother["hobbies"]); // count the numbers of values in an array
+$my_hobbies= count($me["hobbies"]);
+$total=$b_hobbies+$my_hobbies;
+
+array_push($me["hobbies"],"Taxidermy");
+
+$me["Lastname"]="Durand";
+
+$soulmate=array(
+   "Firstname" => "Aicha",
+   "Lastname"  => "Malik",
+   "Age" => 24,
+   "Season" => "Summer",
+   "Soccer" => false,
+   "favorite_movies"=> ["Green Line","The intern","The terminal","Shrek"],
+   "hobbies"=>["Art","Cooking","Reading","Movies"]
+);
+
+// perform array operation
+$possible_hobbies_via_intersection = array_intersect($me["hobbies"],$soulmate["hobbies"]);
+$possible_hobbies_via_merge = array_merge($me["hobbies"],$soulmate["hobbies"]);
+
+$web_development=[
+   "frontend" => [],
+   "backend"  => []
+];
+
+array_push($web_development["frontend"],"xhtml");
+array_push($web_development["backend"],"Ruby on Rails");
+array_push($web_development["frontend"],"CSS");
+array_push($web_development["frontend"],"Flash");
+array_push($web_development["frontend"],"Javascript");
+array_push($web_development["backend"],"Javascript");
+
+
+
+$web_development["frontend"][0] = "html";
+unset($web_development["frontend"][2]);
 
  echo '<pre>';
-print_r($me);
+print_r($web_development);
+
+
+ //  print_r($possible_hobbies_via_intersection);
+//  print_r($possible_hobbies_via_merge);
+ // print_r($total);
+// print_r($me);
 echo '</pre>';
 
 
